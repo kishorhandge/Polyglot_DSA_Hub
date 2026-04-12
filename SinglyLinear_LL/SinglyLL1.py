52052e7cf3883c268833414c098b641e62de4589
kishorhandge <kishorhandge29@gmail.com>
2026-04-12 23:40:35 +0530

    Add files via upload

---
+++ SinglyLinear_LL/SinglyLL1.py
@@ -0,0 +1,99 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class SinglyLL:
+    def __init__(self):
+        self.first = None
+        self.iCount = 0
+
+    def InsertFirst(self,no):
+       
+        newn = Node(no)
+
+        if(self.first == None):
+            self.first = newn
+
+        else:
+            newn.next = self.first
+            self.first = newn
+
+    
+        self.iCount = self.iCount + 1
+
+
+    def InsertLast(self,no):
+
+        newn = Node(no)
+
+        if(self.first == None):
+            self.first = newn
+
+        else:
+            
+            temp = self.first
+
+            while(temp.next != None):
+                temp = temp.next
+
+
+            temp.next = newn
+
+    
+        self.iCount = self.iCount + 1
+
+
+    def DeleteFirst(self):
+        pass
+
+    def DeleteLast(self):
+        pass
+
+    def Display(self):
+        temp = self.first
+
+        while(temp != None):
+            print("| ",temp.data," |->",end=" ")
+            temp = temp.next
+
+        print("None")
+
+    def Count(self):
+        return self.iCount
+
+    def InsertAtPosFirst(self,no,pos):
+        pass
+
+
+    def DeleteAtPos(self,pos):
+        pass
+
+    
+
+
+
+def main():
+
+    sobj = SinglyLL()
+
+    sobj.InsertFirst(101)
+    sobj.InsertFirst(51)
+    sobj.InsertFirst(21)
+    sobj.InsertFirst(11)
+
+    print("Elements of Linked List are :")
+    sobj.Display()
+
+    print("Number of Elements in Linekd List are :",sobj.Count())
+
+    sobj.InsertLast(101)
+    sobj.InsertLast(111)
+
+    print("Elements of Linked List are :")
+    sobj.Display()
+
+    print("Number of Elements in Linekd List are :",sobj.Count())
+
+if __name__ == "__main__":
+    main()

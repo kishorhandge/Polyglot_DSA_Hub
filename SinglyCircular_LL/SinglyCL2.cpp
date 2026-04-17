e0d3cad1f0c9caa64c23b6af5add3ee9fd00d870
kishorhandge <kishorhandge29@gmail.com>
2026-04-17 23:51:36 +0530

    Add files via upload

---
+++ SinglyCircular_LL/SinglyCL2.cpp
@@ -0,0 +1,206 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+
+class SinglyCL
+{
+public:
+    PNODE first;
+    PNODE last;
+    int iCount;
+
+    SinglyCL()
+    {
+        cout << "Objects of singlyCL gets created :" << "\n";
+        first = NULL;
+        last = NULL;
+        iCount = 0;
+    }
+
+    void InsertFirst(int no)
+    {
+        PNODE newn = NULL;
+
+        newn = new NODE;
+
+        newn->data = no;
+        newn->next = NULL;
+
+        if ((first == NULL) && (last == NULL))
+        {
+            first = newn;
+            last = newn;
+        }
+        else
+        {
+            newn->next = first;
+            first = newn;
+        }
+
+
+        last->next = first;
+        iCount++;
+    }
+    void InsertLast(int no)
+    {
+        PNODE newn = NULL;
+
+        newn = new NODE;
+
+        newn->data = no;
+        newn->next = NULL;
+
+        if ((first == NULL) && (last == NULL))
+        {
+            first = newn;
+            last = newn;
+        }
+        else
+        {
+            last->next = newn;
+            last = newn;
+
+        }
+
+        last->next = first;
+        iCount++;
+    }
+    void DeleteFirst()
+    {
+        PNODE temp = NULL;
+
+        if ((first == NULL) && (last == NULL))
+        {
+            return;
+        }
+        else if ((first) == (last))
+        {
+           delete(first);
+            first = NULL;
+        }
+        else
+        {
+            temp = (first);
+
+            (first) = (first)->next;
+
+            delete(temp);
+        }
+
+        last->next = first;
+        iCount--;
+    }
+    void DeleteLast()
+    {
+        PNODE temp = NULL;
+
+        if ((first == NULL) && (last == NULL))
+        {
+            return;
+        }
+        else if ((first) == (last))
+        {
+           delete(first);
+            first = NULL;
+        }
+        else
+        {
+            temp = (first);
+
+            while (temp->next != last)
+            {
+                temp = temp->next;
+            }
+
+            delete(last);
+            
+            last = temp;
+        }
+
+        last->next = first;
+        iCount--;
+    }
+    void Display()
+    {
+        PNODE temp = NULL;
+
+        temp = first;
+
+        do
+        {
+            cout << "| " << temp->data << " |=>";
+            temp = temp->next;
+
+        }while(temp != last->next);
+
+        cout << "\n";
+    }
+
+    int Count()
+    {
+        return iCount;
+    }
+
+    void InsertAtPos(int no, int pos)
+    {
+    }
+
+    void DeleteAtPos(int pos)
+    {
+    }
+};
+
+int main()
+{
+    SinglyCL obj;
+
+    int iRet = 0;
+
+    obj.InsertFirst(51);
+    obj.InsertFirst(41);
+    obj.InsertFirst(31);
+    obj.InsertFirst(21);
+    obj.InsertFirst(11);
+
+    obj.Display();
+
+    iRet = obj.Count();
+
+    cout << "Number of nodes are :" << iRet << "\n";
+
+    obj.InsertLast(101);
+    obj.InsertLast(111);
+
+    obj.Display();
+
+    iRet = obj.Count();
+
+    cout << "Number of nodes are :" << iRet << "\n";
+
+    obj.DeleteFirst();
+
+    obj.Display();
+
+    iRet = obj.Count();
+
+    cout << "Number of nodes are :" << iRet << "\n";
+
+    obj.DeleteLast();
+
+    obj.Display();
+
+    iRet = obj.Count();
+
+    cout << "Number of nodes are :" << iRet << "\n";
+
+
+    return 0;
+}

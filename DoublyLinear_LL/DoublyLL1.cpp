f93e9fcb62d80e11e7402e4afecbc78767ca47c2
kishorhandge <kishorhandge29@gmail.com>
2026-04-19 23:31:49 +0530

    Add files via upload

---
+++ DoublyLinear_LL/DoublyLL1.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+
+class DoublyLL
+{
+public:
+    PNODE first;
+    int iCount;
+
+    DoublyLL()
+    {
+        cout << "Objects of DoublyLL gets created :" << "\n";
+        first = NULL;
+        iCount = 0;
+    }
+
+    void InsertFirst(int no)
+    {
+        PNODE newn = NULL;
+
+        newn = new NODE;
+
+        newn->data = no;
+        newn->next = NULL;
+        newn->prev = NULL;
+
+        if (first == NULL)
+        {
+            first = newn;
+        }
+        else
+        {
+            newn->next = first;
+            first->prev = newn;
+            first = newn;
+        }
+        iCount++;
+    }
+    void InsertLast(int no)
+    {
+        PNODE newn = NULL;
+        PNODE temp = NULL;
+
+        newn = new NODE;
+
+        newn->data = no;
+        newn->next = NULL;
+        newn->prev = NULL;
+
+        if (first == NULL)
+        {
+            first = newn;
+        }
+        else
+        {
+            temp = first;
+
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+
+            temp->next = newn;
+            newn->prev = temp;
+        }
+        iCount++;
+    }
+    void DeleteFirst()
+    {
+    }
+    void DeleteLast()
+    {
+    }
+    void Display()
+    {
+        PNODE temp = NULL;
+
+        temp = first;
+
+        while (temp != NULL)
+        {
+            cout << "| " << temp->data << " |->";
+            temp = temp->next;
+        }
+
+        cout << "NULL\n";
+    }
+
+    int Count()
+    {
+        return iCount;
+    }
+
+    void InsertAtPos(int no, int pos)
+    {
+    }
+
+    void DeleteAtPos(int pos)
+    {
+    }
+};
+
+int main()
+{
+    DoublyLL obj;
+
+    int iRet = 0;
+
+    obj.InsertFirst(51);
+    obj.InsertFirst(41);
+    obj.InsertFirst(31);
+    obj.InsertFirst(21);
+    obj.InsertFirst(11);
+
+    obj.Display();
+
+    iRet = obj.Count();
+
+    cout << "Number of nodes are :" << iRet << "\n";
+
+    obj.InsertLast(101);
+    obj.InsertLast(111);
+
+    obj.Display();
+
+    iRet = obj.Count();
+
+    cout << "Number of nodes are :" << iRet << "\n";
+
+    return 0;
+}

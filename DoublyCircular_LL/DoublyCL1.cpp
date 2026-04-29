4422baa6219e8a0709703ab713c04a53ca358cda
kishorhandge <kishorhandge29@gmail.com>
2026-04-29 22:17:01 +0530

    Add files via upload

---
+++ DoublyCircular_LL/DoublyCL1.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+
+class DoublyCL
+{
+public:
+    PNODE first;
+    PNODE last;
+    int iCount;
+
+    DoublyCL()
+    {
+        cout << "Objects of DoublyCL gets created :" << "\n";
+        first = NULL;
+        last = NULL;
+        iCount = 0;
+    }
+
+    void InsertFirst(int no)
+    {
+        PNODE newn = NULL;
+
+        newn = new NODE;
+
+        newn->data = no;
+        newn->next = NULL;
+        newn->prev = NULL;
+
+        if ((first == NULL) && (last == NULL))
+        {
+            first = newn;
+            last = newn;
+        }
+        else
+        {
+            newn->next = first;
+            first->prev = newn;
+            first = newn;
+        }
+
+        last->next = first;
+        first->prev = last;
+        
+        iCount++;
+    }
+    void InsertLast(int no)
+    {
+        PNODE newn = NULL;
+
+        newn = new NODE;
+
+        newn->data = no;
+        newn->next = NULL;
+        newn->prev = NULL;
+
+        if((first == NULL) && (last == NULL))
+        {
+            first = newn;
+            last = newn;
+        }
+        else
+        {
+            last->next = newn;
+            newn->prev = last;
+
+            last = newn;
+        }
+        
+        last->next = first;
+        first->prev = last;
+        
+        iCount++;
+    }
+    void DeleteFirst()
+    {
+    }
+    void DeleteLast()
+    {
+    }
+    void Display()
+    {
+        PNODE temp = NULL;
+
+        temp = first;
+
+        do
+        {
+            cout << "| " << temp->data << " |<=>";
+            temp = temp->next;
+
+        }while(temp != last->next);
+
+        cout << "\n";
+    }
+
+    int Count()
+    {
+        return iCount;
+    }
+
+    void InsertAtPos(int no, int pos)
+    {
+    }
+
+    void DeleteAtPos(int pos)
+    {
+    }
+};
+
+int main()
+{
+    DoublyCL obj;
+
+    int iRet = 0;
+
+    obj.InsertFirst(51);
+    obj.InsertFirst(41);
+    obj.InsertFirst(31);
+    obj.InsertFirst(21);
+    obj.InsertFirst(11);
+
+    obj.Display();
+
+    iRet = obj.Count();
+
+    cout << "Number of nodes are :" << iRet << "\n";
+
+    obj.InsertLast(101);
+    obj.InsertLast(111);
+
+    obj.Display();
+
+    iRet = obj.Count();
+
+    cout << "Number of nodes are :" << iRet << "\n";
+
+    return 0;
+}
